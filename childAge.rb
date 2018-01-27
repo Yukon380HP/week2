c6d3af605e50e75455bcd61c4624913fe4a4fe84
John Driscoll <driscolljohn84@yahoo.com>
2018-01-27 12:40:28 -0600

    Add files via upload

---
+++ childAge.rb
@@ -0,0 +1,9 @@
+class ChildAge
+  puts "How old are you? "
+  age1 = gets.chomp().to_i
+age1.times {print "Are we there yet? "}
+ # age1 < counter
+ # print "Are we there yet?"
+ #counter += 1
+ #end
+end
