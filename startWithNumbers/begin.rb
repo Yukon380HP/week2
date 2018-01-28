cf8058bb67e83caa10d0cb2e35805d8c678272ed
John Driscoll <driscolljohn84@yahoo.com>
2018-01-28 15:24:01 -0600

    Delete begin.rb

--- startWithNumbers/begin.rb
+++
@@ -1,9 +0,0 @@
-class Begin
-  puts "Please enter the first of two numbers:"
-  num1 = gets.chomp().to_i
-  puts "Second number:"
-  num2 = gets.chomp().to_i
-  puts "The sum is: ",+ (num1 + num2)
-  puts "The difference is: ", + (num1 - num2)
-  puts "The product is: ",  + (num1 * num2)
-end
